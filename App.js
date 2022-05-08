a0f945487656e3877f774844bbe375b7a8226fea
MarcosPauloJS <marcospaulooj@gmail.com>
2022-05-07 22:33:06 -0300

    add react navigation

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
-import { darkTheme, lightTheme  } from './theme';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/screens/Home/index.js';
+import { lightTheme  } from './theme';
+
+
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <ThemeProvider theme={theme === 'dark' ? darkTheme : lightTheme}>''
-      <Text>vila events</Text>
+    <ThemeProvider theme={lightTheme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+     </NavigationContainer>
     </ThemeProvider>
   );
 }
